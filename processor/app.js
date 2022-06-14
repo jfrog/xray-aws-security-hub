8cf1809bff7ac932ce34ab88518039a1f94cbf67
Daniel Miakotkin <danielmi@jfrog.com>
2022-06-13 22:12:31 -0700

    WIP, lambda refactoring

--- processor/app.js
+++ processor/app.js
@@ -1,100 +1,105 @@
+// eslint-disable-next-line import/extensions
+import SQS from 'aws-sdk/clients/sqs.js';
+import _ from 'lodash';
+import { v4 as uuidv4 } from 'uuid';
 
-import SQS from "aws-sdk/clients/sqs.js";
 const sqs = new SQS();
 
-//import split from "split-array"
+const formatError = function (error) {
+  const response = {
+    statusCode: error.statusCode,
+    headers: {
+      'Content-Type': 'text/plain',
+      'x-amzn-ErrorType': error.code,
+    },
+    isBase64Encoded: false,
+    body: `${error.code}: ${error.message}`,
+  };
+  console.error(response);
+  return response;
+};
 
-export async function lambdaHandler(event) {
-    let messagesSent
-    try {
-
-        messagesSent = await createIssuesList(event)
-
-    }catch(error){
-        return formatError(error)
-    }
-    return formatResponse(messagesSent);
-}
+const formatResponse = function (body) {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    isBase64Encoded: false,
+    multiValueHeaders: {
+      'X-Custom-Header': ['JFrog', 'Xray'],
+    },
+    body,
+  };
+  console.log(JSON.stringify(response));
+  return response;
+};
 
 async function createIssuesList(event) {
-    let result = {};
-    let messagesSent = []
+  let result = {};
+  const issuesArray = [];
 
-    let additionalFields = {
-        watch_name: event.watch_name,
-        policy_name: event.policy_name
-    }
+  const additionalFields = {
+    watch_name: event.watch_name,
+    policy_name: event.policy_name,
+  };
 
-    // combine 10 issue in the 'result', then create an array of batches of 10 issues and send it as a body
-
-    try {
-        for (let i = 0; i < event.issues.length; i++) {
-            let issue = event.issues[i]
-            result = {
-                ...issue,
-                ...additionalFields
-            }
-
-            let messageInfo = await sendSQSmessage(result)
-            messagesSent.push({
-                message: JSON.stringify(messageInfo)
-            });
-            console.log(messageInfo)
-        }
-
-    } catch (ex) {
-        console.error('Error parsing API call body: ', ex);
-    }
-
-    return messagesSent;
+  event.issues.forEach(injectFields);
+  function injectFields(issue) {
+    result = {
+      ...issue,
+      ...additionalFields,
+    };
+    issuesArray.push({
+      issue: JSON.stringify(result),
+    });
+  }
+  const lodashSpiltArray = _.chunk(issuesArray, 3);
+  console.log(JSON.stringify(lodashSpiltArray));
+  return lodashSpiltArray;
 }
 
-async function sendSQSmessage(result){
-    //let queueURL = process.env.SQS_QUEUE_URL;
-    let queueURL = "https://sqs.us-west-1.amazonaws.com/096302395721/XraySourceQueue.fifo"
+async function sendSQSmessage(issuesArray) {
+  // let queueURL = process.env.SQS_QUEUE_URL;
+  const queueURL = 'https://sqs.us-west-1.amazonaws.com/096302395721/XraySourceQueue.fifo';
+  const messageResponses = [];
 
-    let params = {
-        MessageAttributes: {
-            "MessageType": {
-                DataType: "String",
-                StringValue: "Issue received from JFrog Xray"
-            },
-        },
-        MessageBody: JSON.stringify(result),
-        MessageDeduplicationId: Date.now().toString(),
-        MessageGroupId: "XrayPayload",
-        QueueUrl: queueURL
+  for (const issue of issuesArray) {
+    const params = {
+      QueueUrl: queueURL,
+      Entries: [],
     };
 
-    return await sqs.sendMessage(params).promise()
+    for (const message of issue) {
+      const myuuid = uuidv4();
+      params.Entries.push({
+        Id: myuuid,
+        MessageAttributes: {
+          MessageType: {
+            DataType: 'String',
+            StringValue: 'Final test',
+          },
+        },
+        MessageDeduplicationId: Date.now().toString(),
+        MessageGroupId: 'XrayPayload',
+        MessageBody: JSON.stringify(message),
+      });
+    }
+    messageResponses.push({
+      message: JSON.stringify(await sqs.sendMessageBatch(params).promise()),
+    });
+  }
+  return messageResponses;
 }
 
-let formatError = function(error) {
-    let response = {
-        "statusCode": error.statusCode,
-        "headers": {
-            "Content-Type": "text/plain",
-            "x-amzn-ErrorType": error.code
-        },
-        "isBase64Encoded": false,
-        "body": error.code + ": " + error.message
-    }
-    console.error(response)
-    return response
-}
-
-let formatResponse = function(body){
-    let response = {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "isBase64Encoded": false,
-        "multiValueHeaders": {
-            "X-Custom-Header": ["My value", "My other value"],
-        },
-        "body": body
-    }
-    console.log(response)
-    return response
+export async function lambdaHandler(event) {
+  let issuesArray;
+  let messagesSent;
+  try {
+    issuesArray = await createIssuesList(event);
+    messagesSent = await sendSQSmessage(issuesArray);
+  } catch (error) {
+    return formatError(error);
+  }
+  return formatResponse(messagesSent);
 }
